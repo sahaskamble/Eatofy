1b93e325506826f160e5d972b50dc89977aacac5
Darker Than Shados <144455766+sahaskamble@users.noreply.github.com>
2024-12-15 19:08:09 +0530

    Delete .next directory

--- .next/server/app/api/hotel/auth/check/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/hotel/auth/check/route.js";
-const runtime = require("../../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_b33706._.js");
-runtime.loadChunk("server/chunks/[root of the server]__462c51._.js");
-runtime.loadChunk("server/chunks/_a489fb._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/hotel/auth/check/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/hotel/auth/check/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
